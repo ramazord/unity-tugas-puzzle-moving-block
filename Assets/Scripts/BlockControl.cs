c1be1f6283de06b8051499f2e8017fde7a288852
Muhammad Ramadhan Rahmat <muhamadramadhanrahmat@gmail.com>
2017-11-29 18:23:58 +0700

    BERES BUAT AJI
    Beres Aji

--- Assets/Scripts/BlockControl.cs
+++ Assets/Scripts/BlockControl.cs
@@ -6,23 +6,74 @@ public class BlockControl : MonoBehaviour {
 
 	private Vector3 screenPoint;
 	private Vector3 offset;
-	// Use this for initialization
+
+    private bool isOnStick;
+    GameObject stickObject;
+    Vector3 curMousePos;
+    Vector3 tempPos;
+    void FixedUpdate()
+    {
+        if (isOnStick)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(stickObject.transform.position.x, transform.position.y, transform.position.z), Time.deltaTime * 3);
+        }
+    }
+
 	void OnMouseDown()
 	{
-		//firstY = transform.position.y;
-		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		////firstY = transform.position.y;
+		//screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		//offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        tempPos = transform.position;
 	}
 
 	void OnMouseDrag() {
-		Vector3 currentScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		Vector3 curPosition = Camera.main.ScreenToWorldPoint (currentScreenPoint) + offset;
-		transform.position = curPosition;
-		Debug.Log ("Mouse drags");
+		//Vector3 currentScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		//Vector3 curPosition = Camera.main.ScreenToWorldPoint (currentScreenPoint) + offset;
+        //transform.position = curPosition;
+		//Debug.Log ("Mouse drags");
+
+        if (isOnStick)
+            DragOnStick();
+        else
+            DragOutStick();
+        
 	}
+
+    void DragOnStick()
+    {
+        curMousePos = Camera.main.ScreenToWorldPoint(new Vector3(0, Input.mousePosition.y, 0));
+        transform.position = new Vector3(stickObject.transform.position.x, curMousePos.y, 0);
+    }
+
+    void DragOutStick()
+    {
+        curMousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        transform.position = new Vector3(curMousePos.x, curMousePos.y, 0);
+    }
 
 	private void OnMouseUp()
 	{
-		transform.position = new Vector3(transform.position.x,transform.position.y, transform.position.z);
+		//transform.position = new Vector3(transform.position.x,transform.position.y, transform.position.z);
+        if (!isOnStick)
+            transform.position = tempPos;
 	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Stick")
+        {
+            isOnStick = true;
+            stickObject = col.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Stick")
+        {
+            isOnStick = false;
+            stickObject = null;
+        }
+    }
 }
--- Assets/Scripts/DeteksiBlock.cs
+++ Assets/Scripts/DeteksiBlock.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class DeteksiBlock : MonoBehaviour {
 
 	void OnTriggerStay2D(Collider2D collider) {
-		Debug.Log ("Enter colider with deteksi block");
-		if (Input.GetMouseButtonDown(0))
-			Debug.Log("Pressed left click.");
+		//Debug.Log ("Enter colider with deteksi block");
+		if (Input.GetMouseButtonDown(0));
+			//Debug.Log("Pressed left click.");
 		
-		if (Input.GetMouseButtonDown(1))
-			Debug.Log("Pressed right click.");
-		
-		if (Input.GetMouseButtonDown(2))
-			Debug.Log("Pressed middle click.");
+		if (Input.GetMouseButtonDown(1));
+			//Debug.Log("Pressed right click.");
+
+        if (Input.GetMouseButtonDown(2)) ;
+			//Debug.Log("Pressed middle click.");
 	}
 }
