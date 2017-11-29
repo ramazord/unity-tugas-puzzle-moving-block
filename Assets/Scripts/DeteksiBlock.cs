69a10eeecb4a3c3647ad9a01c5325134bf0d5559
Muhammad Ramadhan Rahmat <muhamadramadhanrahmat@gmail.com>
2017-11-29 17:17:53 +0700

    Revert "Revert "first commit""
    This reverts commit 122f161dc07cb2a03d000a70873069e8da5c18ad.

---
+++ Assets/Scripts/DeteksiBlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteksiBlock : MonoBehaviour {
+
+	void OnTriggerStay2D(Collider2D collider) {
+		Debug.Log ("Enter colider with deteksi block");
+		if (Input.GetMouseButtonDown(0))
+			Debug.Log("Pressed left click.");
+		
+		if (Input.GetMouseButtonDown(1))
+			Debug.Log("Pressed right click.");
+		
+		if (Input.GetMouseButtonDown(2))
+			Debug.Log("Pressed middle click.");
+	}
+}
